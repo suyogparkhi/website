ce5cc2df99a42a7b81cb013ae3922b7f8f12f564
OmChillure <omchillure4@gmail.com>
2024-10-29 15:49:29 +0530

    feat: updated swap ui

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,14 +1,18 @@
-import React from "react";
+"use client";
+import { Navbar } from "@/components/Navbar";
+import React, { useRef } from "react";
 
 export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <>
-          {children}      
-          </>
-    );
-  }
-  
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const featuresRef = useRef<HTMLElement>(null);
+  const updatesRef = useRef<HTMLElement>(null);
+  return (
+    <>
+      <Navbar featuresRef={featuresRef} updatesRef={updatesRef} />
+      {children}
+    </>
+  );
+}
--- src/app/(dashboard)/swap/(components)/Swap.tsx
+++ src/app/(dashboard)/swap/(components)/Swap.tsx
@@ -3,7 +3,6 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
-  ArrowDownIcon,
   Settings2,
   Wallet,
   ChevronDown,
@@ -14,13 +13,15 @@ import {
   SelectContent,
   SelectItem,
   SelectTrigger,
-  SelectValue,
 } from "@/components/ui/select";
+import { ArrowUpDownIcon } from "lucide-react";
 
 const SwapInterface = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-slate-950 p-4">
-      <div className="flex justify-end w-full max-w-md mb-2">
+    <div className="h-[calc(100vh-150px)] flex flex-col items-center justify-center p-4">
+      <div className="absolute w-[40rem] h-[40rem] bg-gradient-to-br from-violet-600/20 to-blue-600/20 rounded-full blur-3xl animate-pulse pointer-events-none" />
+
+      <div className="flex justify-end w-full max-w-md mb-2 relative">
         <Button
           variant="ghost"
           size="icon"
@@ -30,7 +31,7 @@ const SwapInterface = () => {
         </Button>
       </div>
 
-      <Card className="border-slate-800 bg-slate-900/50 backdrop-blur-sm">
+      <Card className="border-slate-800 bg-slate-900 backdrop-blur-sm relative">
         <CardContent className="p-6">
           <div className="flex-col gap-10">
             <div className="flex items-center gap-4">
@@ -38,13 +39,12 @@ const SwapInterface = () => {
                 <span>Send</span>
                 <Input
                   type="number"
-                  className="w-[20rem] border-0 bg-slate-800 focus:ring-offset-0"
+                  className="w-[20rem] h-[7vh] border-0 bg-slate-800 focus:ring-offset-0"
                   placeholder="Enter amount"
-                  
                 />
               </div>
               <Select defaultValue="eth">
-                <SelectTrigger className="w-[140px] border-0 bg-slate-800 focus:ring-offset-0">
+                <SelectTrigger className="w-[140px] h-[7vh] border-0 bg-slate-800 focus:ring-offset-0">
                   <div className="flex items-center gap-2">
                     <GalleryVerticalEnd className="h-5 w-5" />
                     <span>ETH</span>
@@ -64,7 +64,7 @@ const SwapInterface = () => {
             <div className="mt-7 flex items-center gap-4">
               <span>From</span>
               <Select defaultValue="eth">
-                <SelectTrigger className="w-[30rem] border-0 bg-slate-800 focus:ring-offset-0">
+                <SelectTrigger className="w-[30rem] h-[7vh] border-0 bg-slate-800 focus:ring-offset-0">
                   <div className="flex items-center gap-2">
                     <GalleryVerticalEnd className="h-5 w-5" />
                     <span>ETH</span>
@@ -82,33 +82,36 @@ const SwapInterface = () => {
               </Select>
             </div>
           </div>
-          <hr className="my-6 border-gray-200  dark:border-gray-700 lg:my-8 w-full" />
 
+          <div className="flex justify-center items-center">
+            <ArrowUpDownIcon />
+            <hr className="ml-7 border-gray-200 dark:border-gray-700 lg:my-8 w-full" />
+          </div>
 
           <div className="flex-col gap-10">
             <div className="flex items-center gap-4">
               <div className="flex items-center gap-4">
                 <span>To</span>
                 <Select defaultValue="eth">
-                <SelectTrigger className="w-[20rem] border-0 bg-slate-800 focus:ring-offset-0 ml-5">
-                  <div className="flex items-center gap-2">
-                    <GalleryVerticalEnd className="h-5 w-5" />
-                    <span>ETH</span>
-                  </div>
-                  <ChevronDown className="h-4 w-4 opacity-50" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="eth">
+                  <SelectTrigger className="w-[20rem] h-[7vh] border-0 bg-slate-800 focus:ring-offset-0 ml-5">
                     <div className="flex items-center gap-2">
                       <GalleryVerticalEnd className="h-5 w-5" />
                       <span>ETH</span>
                     </div>
-                  </SelectItem>
-                </SelectContent>
-              </Select>
+                    <ChevronDown className="h-4 w-4 opacity-50" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="eth">
+                      <div className="flex items-center gap-2">
+                        <GalleryVerticalEnd className="h-5 w-5" />
+                        <span>ETH</span>
+                      </div>
+                    </SelectItem>
+                  </SelectContent>
+                </Select>
               </div>
               <Select defaultValue="eth">
-                <SelectTrigger className="w-[140px] border-0 bg-slate-800 focus:ring-offset-0">
+                <SelectTrigger className="w-[140px] h-[7vh] border-0 bg-slate-800 focus:ring-offset-0">
                   <div className="flex items-center gap-2">
                     <GalleryVerticalEnd className="h-5 w-5" />
                     <span>ETH</span>
@@ -125,12 +128,11 @@ const SwapInterface = () => {
                 </SelectContent>
               </Select>
             </div>
-            
 
             <div className="mt-7 flex items-center gap-4">
               <span>Get</span>
               <Select defaultValue="eth">
-                <SelectTrigger className="w-[30rem] border-0 bg-slate-800 focus:ring-offset-0 ml-2.5">
+                <SelectTrigger className="w-[30rem] h-[7vh] border-0 bg-slate-800 focus:ring-offset-0 ml-2.5">
                   <div className="flex items-center gap-2">
                     <GalleryVerticalEnd className="h-5 w-5" />
                     <span>ETH</span>
@@ -151,9 +153,7 @@ const SwapInterface = () => {
         </CardContent>
       </Card>
 
-      <Button
-        className="w-[35rem] max-w-md mt-4 bg-blue-600 hover:bg-blue-700 text-white"
-      >
+      <Button className="w-[36.5rem] h-[8vh] mt-5 bg-gradient-to-r from-[#7371FC] to-[#A594F9] hover:from-[#7371FC] hover:to-[#A594F9] text-white rounded-lg transition-all duration-300 shadow-lg hover:shadow-violet-500/25 backdrop-blur-sm disabled:opacity-50">
         <Wallet className="h-5 w-10 mr-2" />
         Connect Wallet
       </Button>
